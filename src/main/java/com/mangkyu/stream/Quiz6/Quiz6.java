ad7ce1e004921b8b1fe4f2ef42bd493c1fa32758
SBSun <46879319+SBSun@users.noreply.github.com>
2023-10-18 15:15:34 +0900

    Feat: 6.1, 6.2 문제 풀이

--- src/main/java/com/mangkyu/stream/Quiz6/Quiz6.java
+++ src/main/java/com/mangkyu/stream/Quiz6/Quiz6.java
@@ -1,8 +1,11 @@
 package com.mangkyu.stream.Quiz6;
 
+import java.nio.file.Files;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Quiz6 {
 
@@ -31,14 +34,24 @@ public class Quiz6 {
         };
     }
 
-    // stuArr에서 불합격(150점 미만)한 학생의 수를 남자와 여자로 구별하여라. (Boolean, List)
+    // 6.1 stuArr에서 불합격(150점 미만)한 학생의 수를 남자와 여자로 구별하여라. (Boolean, List)
     public Map<Boolean, List<Student>> quiz1() {
-        return new HashMap<>();
+        Map<Boolean, List<Student>> res = Arrays.stream(stuArr)
+                .filter(s -> s.getScore() < 150)
+                .collect(Collectors.groupingBy(Student::isMale));
+
+        return res;
     }
 
-    // 각 반별 총점을 학년 별로 나누어 구하여라 (Map<Integer, Map<Integer, Integer>>)
+    // 6.2 각 반별 총점을 학년 별로 나누어 구하여라 (Map<Integer, Map<Integer, Integer>>)
     public Map<Integer, Map<Integer, Integer>> quiz2() {
-        return new HashMap<>();
+        Map<Integer, Map<Integer, Integer>> res = Arrays.stream(stuArr)
+                .collect(Collectors.groupingBy(
+                        Student::getHak, Collectors.groupingBy(
+                                Student::getBan, Collectors.summingInt(Student::getScore))
+                ));
+
+        return res;
     }
 
 }
